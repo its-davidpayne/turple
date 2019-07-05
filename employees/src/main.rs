cab57b8104611740a183c88b0edd4a177043acf5
its-davidpayne <davidcpayne23@gmail.com>
2019-07-05 09:35:52 +0100

    accepts CLargs

--- employees/src/main.rs
+++ employees/src/main.rs
@@ -4,6 +4,21 @@
 // of all people in a department or all people in the company by deprtment, 
 // sorted alphabetically.
 
+use std::env;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Not yet implented");
+    let args: Vec<String> = env::args().collect();
+
+    let mut employee_table: HashMap<String, Vec<String>> = HashMap::new();
+    employee_table.insert(String::from("Sales"), vec![String::from("Aaron Andrews"), String::from("Danny Diller")]);
+    employee_table.insert(String::from("IT"), vec![String::from("Billy Babbish"), String::from("Erin Euler")]);
+    employee_table.insert(String::from("Engineering"), vec![String::from("Claire Cullen"), String::from("Fran Freud")]);
+
+    if args.len() == 1 { // if no args supplied, assume user wants printout of entire table
+        println!("{:#?}", employee_table);
+    } else {
+        println!("Hello");
+    }
 }
+
