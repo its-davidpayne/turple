b1b809166f5fd2c93ace4fca369bf595e63780bd
its-davidpayne <davidcpayne23@gmail.com>
2019-07-24 11:50:39 +0100

    test adjustments

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -32,9 +32,22 @@ impl Guess {
     }
 }
 
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
 
     #[test]
     fn larger_can_hold_smaller() {
